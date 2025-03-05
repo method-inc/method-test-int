f76e76969c834e859f26a9a878addf96b543c7f8
Brent Session <brent.sessions@method.com>
2025-03-05 09:06:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tasks
+1. Run the tests
+2. Fix any failing tests
+3. Identify and discuss changes that could be made to optimize or improve the existing tests.
+
+## Setup
+Prior to meeting, make sure you have [NodeJS](https://nodejs.org/en) installed and have some sort of IDE or code editor installed.
+
+Install Dependencies
+`npm install`
+
+Install Test Browser
+`npx playwright install chromium`
+
+## Run Tests
+Run tests
+`npm run test`
+
+Run tests in visible browser
+`npm run test -- --headed`
---
+++ tests/form-submit.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('submit contact form successfully', async ({ page }) => {
+  await page.goto( 'https://automationintesting.online/');
+  await page.waitForTimeout( 3000 );
+
+  await page.getByLabel( 'Name' ).fill( 'Super Tester' );
+  await page.getByLabel( 'Email' ).fill( 'super@fakeemail.com' );
+  await page.getByLabel( 'Phone' ).fill( '18655555555' );
+  await page.getByTestId( 'ContactSubject' ).fill( 'I want to stay here!' );
+  await page.locator( '.description' ).fill( 'I want to come visit, when are\
+    you open and can I bring my pet iguana? ');
+  await page.getByRole( 'button', { name: 'Submit' } ).click();
+});
+
+test('submit contact form unsuccessfully', async ({ page }) => {
+  await page.goto( 'https://automationintesting.online/');
+  await page.waitForTimeout( 3000 );
+
+  await page.getByLabel( 'Name' ).fill( 'Super Tester' );
+  await page.getByLabel( 'Email' ).fill( 'super@fakeemail.com' );
+  await page.getByLabel( 'Phone' ).fill( '8655555555' );
+  await page.getByTestId( 'ContactSubject' ).fill( 'I want to stay here!' );
+  await page.locator( '.description' ).fill( 'I want to come visit' );
+  await page.getByRole( 'button', { name: 'Submit' } ).click();
+});
